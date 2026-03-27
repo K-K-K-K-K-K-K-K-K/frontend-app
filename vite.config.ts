3b35c12b4d407dbbd56a68ba2670cd465c0d5032
kunanon <kunanonsiriadull@gmail.com>
2026-03-27 14:42:46 +0700

    hotfix: edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/frontend-app/', /// Replace with your GitHub repo name
+  // base: '/frontend-app/', /// Comment if deploy via NPM or Docker. Github Repo Name
   plugins: [react()],
 })
