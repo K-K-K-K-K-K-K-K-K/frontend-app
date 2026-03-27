566ba68c7c8e24d63305e192ddc1499f04d95a29
kunanon <kunanonsiriadull@gmail.com>
2026-03-27 10:07:38 +0700

    hotfix: edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/frontend-app/', // comment when deploy locally with npm
   plugins: [react()],
 })
